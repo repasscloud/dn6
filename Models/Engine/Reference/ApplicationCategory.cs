4213a1b80f59328c1d5fdf296ceed3699455d4ab
danijeljw-rpc <danijel@repasscloud.com>
2022-03-18 02:36:18 +1100

    convert from enum to class

--- Models/Engine/Reference/ApplicationCategory.cs
+++ Models/Engine/Reference/ApplicationCategory.cs
@@ -1,9 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace Models.Engine.Reference;
 
-public enum ApplicationCategory
+[Index(nameof(Category), IsUnique = true)]
+public class ApplicationCategory
 {
-    Productivity,
-    Microsoft,
-    Security
+    public int Id { get; set; }
+    [Required]
+    public string Category { get; set; }
 }
