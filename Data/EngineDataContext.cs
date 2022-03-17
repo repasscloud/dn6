9c9ef548925efe78f288ae089e06c453414fe484
danijeljw-rpc <danijel@repasscloud.com>
2022-03-18 03:06:58 +1100

    add DbSet for each context

--- Data/EngineDataContext.cs
+++ Data/EngineDataContext.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Models.Engine.Public;
 using Models.Engine.Reference;
+using Models.Engine.Private;
 
-namespace ApplicationData
+
+namespace EngineData
 {
     public class EngineDataContext : DbContext
     {
@@ -12,5 +14,15 @@ namespace ApplicationData
         }
 
         public DbSet<Application> Applications { get; set; }
+
+        public DbSet<ExploitReport> ExploitReports { get; set; }
+        public DbSet<VirusTotalScan> VirusTotalScans { get; set;}
+
+        public DbSet<ApplicationCategory> ApplicationCategories { get; set; }
+        public DbSet<BaseImage> BaseImages { get; set; }
+        public DbSet<BaseImageEdition> BaseImageEditions { get; set; }
+        public DbSet<BaseImageFileType> BaseImageFileTypes { get; set; }
+        public DbSet<Executable> Executables { get; set; }
+        public DbSet<UninstallProcess> UninstallProcesses { get; set; }
     }
 }
---
+++ Data/SharedDataContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Models.Shared;
+
+namespace SharedData
+{
+    public class SharedDataContext : DbContext
+    {
+        public SharedDataContext (DbContextOptions<SharedDataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CpuArch> CpuArches { get; set; }
+        public DbSet<DetectionProcess> DetectionProcesses { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Locale> Locales { get; set; }
+        public DbSet<PackageDetection> PackageDetections { get; set; }
+        public DbSet<TransferMethod> TransferMethods { get; set; }
+    }
+}
