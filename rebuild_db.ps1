742d762bdd54033efca97c2026ccc2e7cf1145a9
danijeljw-rpc <danijel@repasscloud.com>
2022-03-23 08:21:35 +1100

    clear cache

--- rebuild_db.ps1
+++
@@ -1,8 +0,0 @@
-# list of contexts
-[System.String[]]$Contexts = 'SharedDataContext','EngineDataContext'
-
-foreach ($context in $Contexts)
-{
-    dotnet ef migrations add "AddTables${context}" --context $context
-    dotnet ef database update --context $context
-}
